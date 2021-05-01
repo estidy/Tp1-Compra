958ea1d06768143f739dbab61e6f02b1e405bf93
estidy <estidy@gmail.com>
2021-05-01 02:20:50 -0300

    menu para la compra

--- src/Compra/Main.java
+++ src/Compra/Main.java
@@ -18,7 +18,8 @@ public class Main {
 				double valorUnitario;
 				
 				//Declaracion de variables para un Comprador.
-				String razonSocial, domicilio;
+				String razonSocial;
+				String domicilio;
 				
 				//Declaracion e inicializacion de las variables total y subtotal y contador referente a la compra.
 				double subtotal=0;
@@ -27,7 +28,45 @@ public class Main {
 				
 				Date fecha= new Date();		
 				SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-
-	}
-
+				char opcion;
+				boolean hayProductos;
+				
+				do {
+						System.out.println("i: para iniciar una compra");
+						System.out.println("s: para salir");
+						
+						System.out.println("Ingrese su opcion...");
+						opcion = scan.next().charAt(0); 
+						
+						if (opcion == 's') {
+							 System.out.println("Gracias por utilizar el sistema"); 
+							 System.exit(0);
+						}
+						else if(opcion == 'i') {							
+			                 System.out.println("Ingrese la razon social del Comprador"); 
+			 	             razonSocial = scan.nextLine();
+			 	             
+			 	             System.out.println("Ingrese el domicilio del Comprador"); 
+			 	             domicilio = scan.nextLine();
+			 	             
+			 	             hayProductos = true;
+			 	             System.out.println("Se procederá con la carga de productos...");
+			                 while(hayProductos) {
+					               	 System.out.println("Ingrese la descripcion del producto"); 
+					               	 descripcion = scan.nextLine();
+					               	 System.out.println("Ingrese la cantidad del producto"); 
+					               	 cantidad = scan.nextInt();
+					               	 System.out.println("Ingrese el valor unitario del producto"); 
+					               	 valorUnitario = scan.nextDouble();
+					               	 System.out.println("Producto agregado exitosamente");
+			                 }
+						}
+			            else {
+			            	
+			            	System.out.println("El valor ingresado es incorrecto");
+			            }
+		               	 
+				 }
+				 while (opcion != 's');
+      }
 }
