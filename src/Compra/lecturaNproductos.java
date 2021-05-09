dcf86e671ce18a17f5c2a0ce183cd1349818c299
estidy <estidy@gmail.com>
2021-05-08 22:33:19 -0300

    creacion de la clase lecturaNproductos

---
+++ src/Compra/lecturaNproductos.java
@@ -0,0 +1,18 @@
+package Compra;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class lecturaNproductos {
+
+	public static void main(String[] args) {
+		// Ingresar los datos de una compra (2 productos),
+		// y los datos del comprador, para emitir la factura de compra.
+		Scanner scan = new Scanner(System.in);
+				
+
+	}
+
+}
