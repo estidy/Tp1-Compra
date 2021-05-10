01f4e86a51ecdfacb9831907423ab2d80cdafaee
estidy <estidy@gmail.com>
2021-05-10 14:16:56 -0300

    declaracion de variables

--- src/Compra/lecturaNproductos.java
+++ src/Compra/lecturaNproductos.java
@@ -10,8 +10,31 @@ public class lecturaNproductos {
 	public static void main(String[] args) {
 		// Ingresar los datos de una compra (con n productos),
 		// y para n compradores, para emitir la factura de compra.
+		
 		Scanner scan = new Scanner(System.in);
+		
+		// Declaracion de la constante IVA
+		final double IVA = 0.21;
+		
+		//Declaracion de variables para  Productos.
+		int cantidad;
+		String descripcion;
+		double valorUnitario;		
+		
+		//Declaracion de variables para un Comprador.
+		String razonSocial, domicilio;
+		
+		//Declaracion de las variables total y subtotal y valorIVA referente a la compra.
+		double subtotal, total, valorIVA; 
+		
+		// Declaracion de otras variables
+		DecimalFormat d = new DecimalFormat("##.00");
+		
+		Date fecha= new Date();		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		
 		String pregunta;
+		
 		//Evaluar si la caja no esta vacia
 		pregunta = "Hay compradores en la caja? s/n.";
 		while(respuestaCorrecta(scan, pregunta)) {
@@ -38,7 +61,7 @@ public class lecturaNproductos {
 				
 	}
 	
-	//funciones
+	//métodos
 	
 	private static boolean respuestaCorrecta(Scanner scan, String pregunta) {
 		System.out.println("\n" + pregunta + "\n");
@@ -53,6 +76,17 @@ public class lecturaNproductos {
 	
 	private static boolean opcionValida(char respuesta) {	
 			return((respuesta == 's' || respuesta == 'n')?true:false);
+	}
+	
+	private static String(Scanner scan) {
+		System.out.println("\n" + pregunta + "\n");
+		char respuesta= scan.next().charAt(0);	
+		if(opcionValida(respuesta)) {
+			return((respuesta == 's')?true:false);
+		}
+		else {
+			return respuestaCorrecta(scan, pregunta);
+		}
 	}
 
 }
