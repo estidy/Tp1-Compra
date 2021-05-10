1a7bcb265d5f4f4ad214d8070c308f70a2300966
estidy <estidy@gmail.com>
2021-05-10 16:08:26 -0300

    definiendo metodos leerComprador(Scanner scan), leerProducto(Scanner scan) y calcularSubtotal(cantidad, valorUnitario)

--- src/Compra/lecturaNproductos.java
+++ src/Compra/lecturaNproductos.java
@@ -6,7 +6,9 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class lecturaNproductos {
-
+	
+	public static double subtotal=0;
+	
 	public static void main(String[] args) {
 		// Ingresar los datos de una compra (con n productos),
 		// y para n compradores, para emitir la factura de compra.
@@ -14,18 +16,11 @@ public class lecturaNproductos {
 		Scanner scan = new Scanner(System.in);
 		
 		// Declaracion de la constante IVA
-		final double IVA = 0.21;
-		
-		//Declaracion de variables para  Productos.
-		int cantidad;
-		String descripcion;
-		double valorUnitario;		
-		
-		//Declaracion de variables para un Comprador.
-		String razonSocial, domicilio;
-		
+		final double IVA = 0.21;	
+						
 		//Declaracion de las variables total y subtotal y valorIVA referente a la compra.
-		double subtotal, total, valorIVA; 
+		
+		double total, valorIVA; 
 		
 		// Declaracion de otras variables
 		DecimalFormat d = new DecimalFormat("##.00");
@@ -43,14 +38,16 @@ public class lecturaNproductos {
 			String comprador = leerComprador(scan);
 			
 			//Evaluar si hay productos en la caja para iniciar la compra.
-			String productos;
+
 			pregunta = "Hay productos en la caja? s/n.";
+			String productos="";
+			
 			while(respuestaCorrecta(scan, pregunta)){
 				//Procesar productos
 				String producto = leerProducto(scan);
-				productos += (producto + "-");
+				productos += producto;
 			}
-			if(!respuestaCorrecta(scan, pregunta)
+			if(!respuestaCorrecta(scan, pregunta))
 				 System.out.println("No hay productos para iniciar la compra");
 			
 			// Imprimir factura
@@ -78,15 +75,39 @@ public class lecturaNproductos {
 			return((respuesta == 's' || respuesta == 'n')?true:false);
 	}
 	
-	private static String(Scanner scan) {
-		System.out.println("\n" + pregunta + "\n");
-		char respuesta= scan.next().charAt(0);	
-		if(opcionValida(respuesta)) {
-			return((respuesta == 's')?true:false);
-		}
-		else {
-			return respuestaCorrecta(scan, pregunta);
-		}
+	private static String leerComprador(Scanner scan){
+		 System.out.println("************DATOS DEL COMPRADOR************"); 
+		 
+	     System.out.println("Razon Social: "); 
+	     String razonSocial = scan.nextLine();			 	             
+	     
+	     System.out.println("Domicilio: "); 
+	     String domicilio = scan.nextLine();
+	     
+	     return(razonSocial + ";" + domicilio + ".");
+	}
+	
+	private static String leerProducto(Scanner scan){
+		 System.out.println("************DATOS DEL PRODUCTO************"); 
+		 
+		 System.out.println("Descripcion: "); 
+	   	 String descripcion = scan.nextLine();
+	   	 
+	   	 System.out.println("Cantidad: "); 
+	   	 int cantidad = scan.nextInt();
+	   	 
+	     scan.nextLine();
+	   	 
+	   	 System.out.println("Valor Unitario: "); 
+	   	 double valorUnitario = scan.nextDouble();
+	   	 
+	     calcularSubtotal(cantidad, valorUnitario);
+	     
+	     return(descripcion + ";" + cantidad + ";" + valorUnitario + ".");
+	}
+	
+	private static void calcularSubtotal(int cantidad, double valorUnitario){
+		subtotal += (cantidad * valorUnitario);
 	}
 
-}
+	}
