ce85d5d1e7ba88aef88d31219c384450cedbbbaf
estidy <estidy@gmail.com>
2021-05-10 13:04:45 -0300

    definiendo los metodos respuestaCorrecta(scan,pregunta) y opcionValida(respuesta) para consultar si el usuario elige si o no cuando hay compradores en la caja y cuando hay productos para iniciar la compra.

--- src/Compra/lecturaNproductos.java
+++ src/Compra/lecturaNproductos.java
@@ -11,8 +11,47 @@ public class lecturaNproductos {
 		// Ingresar los datos de una compra (con n productos),
 		// y para n compradores, para emitir la factura de compra.
 		Scanner scan = new Scanner(System.in);
+		
+		//Evaluar si la caja no esta vacia
+		String pregunta = "Hay compradores en la caja? s/n.";
+		while(respuestaCorrecta(scan, pregunta)) {
+			
+			// Procesar comprador
+			String comprador = leerComprador(scan);
+			
+			//Evaluar si hay productos en la caja para iniciar la compra.
+			String productos;
+			while(hayProductos) {
+				//Procesar productos
+				String producto = leerProducto(scan);
+				productos += (producto + "-");
+			}
+			if(!(hayProductos(scan)))
+				 System.out.println("No hay productos para iniciar la compra");
+			
+			// Imprimir factura
+			
+		}
+		if(!respuestaCorrecta(scan, pregunta))
+			 System.out.println("No hay compradores en la caja - CAJA VACIA");
 				
-
+	}
+	
+	//funciones
+	
+	private static boolean respuestaCorrecta(Scanner scan, String pregunta) {
+		System.out.println("\n" + pregunta + "\n");
+		char respuesta= scan.next().charAt(0);	
+		if(opcionValida(respuesta)) {
+			return((respuesta == 's')?true:false);
+		}
+		else {
+			return respuestaCorrecta(scan, pregunta);
+		}
+	}
+	
+	private static boolean opcionValida(char respuesta) {	
+			return((respuesta == 's' || respuesta == 'n')?true:false);
 	}
 
 }
